d3a2c03c20b363293f664d20993766bb6c43a003
didolehbal <habilmehdi96@gmail.com>
2020-06-20 20:32:08 +0100

    moved routes to /pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
 import { BrowserRouter } from "react-router-dom";
-import Routes from "./components/Routes";
+import Routes from "./pages/Routes";
 import 'antd/dist/antd.css';
 import{Provider} from "react-redux"
 import store from "./redux"
--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import Home from "../pages/Home"
-import Student from '../pages/Student';
-import Module from '../pages/module';
-import FilesList from '../pages/files';
-import TasksTest from '../pages/tasks';
-import Settings from '../pages/settings';
-import StudentTask from '../pages/StudentTask';
-import Filieres from '../pages/Filieres';
-import ScreenShare from "../pages/ScreenShare"
-import FiliereListStudent from '../pages/FiliereListStudent';
+import Home from "./Home"
+import Student from './Student';
+import Module from './module';
+import FilesList from './files';
+import TasksTest from './tasks';
+import Settings from './settings';
+import StudentTask from './StudentTask';
+import Filieres from './Filieres';
+import ScreenShare from "./ScreenShare"
+import FiliereListStudent from './FiliereListStudent';
 function Routes() {
     return (
         <Switch>
