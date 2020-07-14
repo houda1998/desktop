257f52d99ea3f526b3dc5a83089b1f5ab1a82d8a
didolehbal <habilmehdi96@gmail.com>
2020-07-14 20:31:57 +0100

    done solutions

--- src/pages/StudentTask.tsx
+++ src/pages/StudentTask.tsx
@@ -10,37 +10,34 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchAll } from '../redux/actions/models';
 import { RequestQueryBuilder, CondOperator } from '@nestjsx/crud-request';
 import { useParams } from 'react-router-dom';
-const { Countdown } = Statistic;
 function StudentTask() {
-  const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30;
   const dispatch = useDispatch()
   const {taskId} = useParams()
 
-  const solutions = useSelector((state:any) => state.solutions)
+  const solutions = useSelector((state:any) => state.models.solutions)
+  const task = useSelector((state:any) => state.models.tasks.filter((t:any)=>t.id==taskId)[0])
   useEffect(() => {
     const query = RequestQueryBuilder.create()
         query.setFilter({ field: "task.id", operator: CondOperator.EQUALS, value: taskId })
+        console.log(query.query())
     dispatch(fetchAll("solutions",query.query()))
   }, [])
-  
+  console.log(solutions)
     return (
    <MyLayout>
     <Layout>
     <Header style={{background:"white",border:"solid 1px #F0F0F0"}}>
-    <p className="title" style={{float:"left",display:"inline-block"}}>TestTitle</p>
-    <p style={{display:"inline-block",float:"right"}}><Countdown  value={deadline}  style={{marginTop: "11px",
-      marginLeft: "15px",
-      border: "1px #f0f0f0 solid"}}/></p>  
-    <p style={{display:"inline-block",float:"right"}}><Time/>Time Left: </p>  
+    <p className="title" style={{float:"left",display:"inline-block"}}>{task?.title}</p>
    
     </Header>
     <Layout>
       <Content style={{background:"white"}}>
         <Table 
-        columns={[{dataIndex:"user.name", title:"name"}, 
+        columns={[
+        {dataIndex:"user", title:"name", render:(v,d,i)=>(<div>{v.name}</div>)}, 
         {dataIndex:"createdAt", title:"submitted at"}, 
-        {dataIndex:"document.url",title:"document"},
-        {dataIndex:"note",title:"grade"}
+        {dataIndex:"document",title:"document",render:(v,d,i)=>(<a href={`http://localhost:3009/documents/files/${v?.url}`} target="_blank" download>soution</a>)},
+        {dataIndex:"note",title:"grade",render:(v,d,i)=>(<div>{v || "not assigned"}</div>)}
       ]}
         dataSource={solutions}
         />
